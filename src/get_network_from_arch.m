466dcf4a581eb4dd729d3c2d2b70b2792774db88
rjliao <rjliao@cs.toronto.edu>
2016-04-12 22:35:13 -0400

    add some comments

--- src/get_network_from_arch.m
+++ src/get_network_from_arch.m
@@ -1,4 +1,19 @@
 function [network_forward, network_backward] = get_network_from_arch(network_arch)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Build Neural Network 
+%
+% Input: 
+%   network_arch: L x 1, cell array of layer names
+%
+% Output:
+%   network_forward: L x 1, cell array of function handles for forward
+%                    computation
+%   network_backward: L x 1, cell array of function handles for backward
+%                    computation
+%
+% Author:
+%   Renjie Liao
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 num_layer           = length(network_arch);
 network_forward     = cell(num_layer, 1);
--- src/network_update.m
+++ src/network_update.m
@@ -1,5 +1,23 @@
 function [W, dW_old] = network_update(W, dW, dW_old, training_para, iter)
-    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Update Parameters of Neural Network
+%
+% Input: 
+%   W: parameters of neural network
+%   dW: change of parameters (typically gradient)
+%   dW_old: last change of parameters
+%   training_para: hyper-parameters for updating, e.g. learnging
+%                  rate, momentum
+%   iter: iteration number of training
+%
+% Output:
+%   W: updated parameters
+%   dW_old: updated change of parameters
+%
+% Author:
+%   Renjie Liao
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 lr = training_para.layer_lr; 
 momentum = training_para.layer_momentum;
 weight_decay = training_para.layer_weight_decay;
